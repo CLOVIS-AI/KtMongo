5a24380a604935aa1e16bba1c813af4cd711c794
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2024-10-26 00:45:41 +0200

    docs(dsl): Document how to create custom operators

--- dsl/src/commonMain/kotlin/expr/common/Expression.kt
+++ dsl/src/commonMain/kotlin/expr/common/Expression.kt
@@ -20,6 +20,7 @@ import opensavvy.ktmongo.bson.BsonContext
 import opensavvy.ktmongo.bson.BsonFieldWriter
 import opensavvy.ktmongo.bson.buildBsonDocument
 import opensavvy.ktmongo.dsl.LowLevelApi
+import opensavvy.ktmongo.dsl.expr.PredicateExpression
 
 /**
  * A node in the BSON AST.
@@ -58,6 +59,9 @@ interface Expression {
 	@LowLevelApi
 	fun simplify(): Expression?
 
+	/**
+	 * Writes the result of [simplifying][simplify] this expression into [writer].
+	 */
 	@LowLevelApi
 	fun writeTo(writer: BsonFieldWriter)
 
@@ -71,11 +75,65 @@ interface Expression {
 
 /**
  * Utility implementation for [Expression], which handles the [context], [toString] representation and [freezing][freeze].
+ *
+ * ### Implementing a new operator
+ *
+ * Instances of this class are BSON operators, like `$eq`, `$xor`, `$setOnInsert` and `$lookup`.
+ *
+ * **Custom operators bypass the entirety of the safety features provided by this library.
+ * Because they are able to write arbitrary BSON, no checks whatsoever are possible.
+ * If you are not careful, this may make injection attacks or data leaking possible.**
+ *
+ * Before writing your own operator, familiarize yourself with the documentation of [Expression], [AbstractExpression],
+ * [CompoundExpression] and [AbstractCompoundExpression], as well as [BsonFieldWriter].
+ *
+ * Fundamentally, an operator is anything that is able to [write] itself into a BSON document.
+ * Operators should not be mutable, except through their [accept][CompoundExpression.accept] method (if they have one).
+ *
+ * An operator generally looks like the following:
+ * ```kotlin
+ * @LowLevelApi
+ * private class TypePredicateExpressionNode(
+ *     val type: BsonType,
+ *     context: BsonContext,
+ * ) : AbstractExpression(context) {
+ *
+ *     override fun write(writer: BsonFieldWriter) {
+ *         writer.writeInt32("\$type", type.code)
+ *     }
+ * }
+ * ```
+ * The [BsonContext] is required at construction because it is needed to implement [toString], which the user could call at any time,
+ * including while the operator is being constructed (e.g. when using a debugger). It is extremely important that the
+ * `toString` representation they see is consistent with the final BSON sent over the wire.
+ *
+ * Once you have created your operator, use the [accept][CompoundExpression.accept] method to register it into a DSL:
+ * ```kotlin
+ * collection.find {
+ *     User::name {
+ *         accept(TypePredicateExpressionNode(BsonType.Undefined))
+ *     }
+ * }
+ * ```
+ *
+ * Of course, the operator described above is already made available: [PredicateExpression.hasType].
+ *
+ * **Note that if your operator accepts a variable number of sub-expressions (e.g. `$and`), you must ensure that it works for any
+ * number of expressions, including 1 and 0.** See [simplify].
+ *
+ * To create an operator that can accept multiple children operators (for example `$and`), implement [AbstractCompoundExpression].
+ *
+ * Since operators are complex to write, risky to get wrong, and hard to test, we highly recommend to upstream any
+ * operator you create so they can benefit from future fixes. Again, **an improperly-written operator may allow data
+ * corruption or leaking**.
  */
 abstract class AbstractExpression(
 	protected val context: BsonContext,
 ) : Expression {
 
+	/**
+	 * `true` if this expression is immutable.
+	 */
 	protected var frozen: Boolean = false
 		private set
 
@@ -84,6 +142,12 @@ abstract class AbstractExpression(
 		frozen = true
 	}
 
+	/**
+	 * Called when this operator should be written to a [writer].
+	 *
+	 * Note that this function is only called on instances that have passed through [simplify],
+	 * so it is guaranteed that this expression is fully simplified already.
+	 */
 	@LowLevelApi
 	protected abstract fun write(writer: BsonFieldWriter)
 
