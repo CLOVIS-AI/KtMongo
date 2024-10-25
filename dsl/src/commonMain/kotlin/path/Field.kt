12979646a358994ba498ab6fa5115f3681247050
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2024-10-26 01:01:47 +0200

    docs(dsl): Improve documentation of the path package

--- dsl/src/commonMain/kotlin/path/Field.kt
+++ dsl/src/commonMain/kotlin/path/Field.kt
@@ -22,7 +22,7 @@ import opensavvy.ktmongo.dsl.LowLevelApi
 import kotlin.reflect.KProperty1
 
 /**
- * A pointer to a specific field in a document.
+ * High-level, typesafe pointer to a specific field in a document.
  *
  * This type may refer to fields nested in children documents or arrays.
  * Instances are generally used to refer to a MongoDB field we want to read or update, when
@@ -169,7 +169,7 @@ operator fun <Root, Type> Field<Root, Collection<Type>>.get(index: Int): Field<R
 	FieldImpl<Root, Type>(this.path / PathSegment.Indexed(index))
 
 /**
- * DSL to refer to [fields][Field].
+ * DSL to refer to [fields][Field], usually automatically added into scope by operators.
  */
 interface FieldDsl {
 
