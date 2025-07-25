bbefd12279432f3c6d1c9ef70e81e9b3b19133af
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2025-07-25 20:57:37 +0200

    test(bson-multiplatform): Migrate to TestBalloon

--- bson-multiplatform/build.gradle.kts
+++ bson-multiplatform/build.gradle.kts
@@ -17,7 +17,7 @@
 plugins {
 	alias(opensavvyConventions.plugins.base)
 	alias(opensavvyConventions.plugins.kotlin.library)
-	alias(libsCommon.plugins.kotest)
+	alias(libsCommon.plugins.testBalloon)
 }
 
 kotlin {
@@ -54,7 +54,8 @@ kotlin {
 
 	sourceSets.commonTest.dependencies {
 		implementation(projects.bsonTests)
-		implementation(libs.prepared.kotest)
+		implementation(libsCommon.opensavvy.prepared.testBalloon)
+		implementation(libsCommon.kotest.assertions)
 	}
 }
 
