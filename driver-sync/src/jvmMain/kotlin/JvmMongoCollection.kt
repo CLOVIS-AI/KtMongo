dc83855cb450db30bf27fd6e66e2eabe97e1670b
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2026-02-22 16:19:33 +0100

    fix(driver-sync): Fix the driver not using the KtMongo codecs

--- driver-sync/src/jvmMain/kotlin/JvmMongoCollection.kt
+++ driver-sync/src/jvmMain/kotlin/JvmMongoCollection.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024-2025, OpenSavvy and contributors.
+ * Copyright (c) 2024-2026, OpenSavvy and contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,7 +52,7 @@ import java.util.concurrent.TimeUnit
  * To convert an existing MongoDB iterable into an instance of this class, see [asKtMongo].
  */
 class JvmMongoCollection<Document : Any> internal constructor(
-	private val inner: com.mongodb.kotlin.client.MongoCollection<Document>,
+	inner: com.mongodb.kotlin.client.MongoCollection<Document>,
 	nameStrategy: PropertyNameStrategy,
 ) : MongoCollection<Document> {
 
@@ -65,6 +65,9 @@ class JvmMongoCollection<Document : Any> internal constructor(
 		objectIdGenerator = ObjectIdGenerator.Jvm(),
 		nameStrategy = nameStrategy,
 	)
+
+	@OptIn(LowLevelApi::class)
+	private val inner = inner.withCodecRegistry(context.codecRegistry)
 
 	// region Find
 
