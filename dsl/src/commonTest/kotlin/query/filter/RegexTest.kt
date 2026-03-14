e6dbf813f289bbbda8e6db1d3255199b7cf6683d
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2026-03-15 00:57:44 +0100

    test(dsl): Document that the RegExp syntax isn't identical to the documentation, yet is still correct

--- dsl/src/commonTest/kotlin/query/filter/RegexTest.kt
+++ dsl/src/commonTest/kotlin/query/filter/RegexTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025, OpenSavvy and contributors.
+ * Copyright (c) 2025-2026, OpenSavvy and contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,45 @@ package opensavvy.ktmongo.dsl.query.filter
 import opensavvy.ktmongo.dsl.query.shouldBeBson
 import opensavvy.prepared.runner.testballoon.preparedSuite
 
+/**
+ * The examples [in the documentation](https://www.mongodb.com/docs/manual/reference/operator/query/regex/) use syntax:
+ * ```MongoDB-JSON
+ * {
+ *    "field": {
+ *        "$regex": "<pattern>",
+ *        "$options": "<options>"
+ *    }
+ * }
+ * ```
+ * and
+ * ```MongoDB-JSON
+ * {
+ *    "field": {
+ *        "$regex": /<pattern>/<options>
+ *    }
+ * }
+ * ```
+ *
+ * The first pattern uses a nested document, the second uses the native BSON RegExp type.
+ * However, the syntax shown isn't part of the BSON standard, it is specific to the JS drivers.
+ *
+ * According to the [official BSON test suite](https://github.com/mongodb/specifications/blob/master/source/bson-corpus/tests/regex.json),
+ * the native RexExp should be displayed:
+ * ```MongoDB-JSON
+ * {
+ *     "field": {
+ *         "$regex": {
+ *             "$regularExpression": {
+ *                 "pattern": "<pattern>",
+ *                 "options": "<options>"
+ *             }
+ *         }
+ *     }
+ * }
+ * ```
+ * However, this is misleading because it looks like a nested object, when it isn't, it's the JSON representation of the native type.
+ * Copy-pasting this JSON request into a MongoDB editor will not work.
+ */
 val RegexTest by preparedSuite {
 	suite("Regex") {
 		test("Basic usage") {
