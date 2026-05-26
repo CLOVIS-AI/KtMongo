065208cdc1b9e9f49901a6193ec7b5c103f9f900
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2026-05-26 22:39:30 +0200

    feat(driver-multiplatform): Create the MongoClient constructor

--- driver-multiplatform/src/commonMain/kotlin/MongoClient.kt
+++ driver-multiplatform/src/commonMain/kotlin/MongoClient.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025, OpenSavvy and contributors.
+ * Copyright (c) 2025-2026, OpenSavvy and contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,9 +14,72 @@
  * limitations under the License.
  */
 
+package opensavvy.ktmongo.multiplatform
+
+import kotlinx.coroutines.Job
+import opensavvy.ktmongo.bson.multiplatform.BsonFactory
+import opensavvy.ktmongo.bson.types.ObjectId
+import opensavvy.ktmongo.bson.types.ObjectIdGenerator
+import opensavvy.ktmongo.dsl.BsonContext
+import opensavvy.ktmongo.dsl.LowLevelApi
+import opensavvy.ktmongo.dsl.path.PropertyNameStrategy
+import opensavvy.ktmongo.multiplatform.wire.MongoWireClient
+import kotlin.concurrent.atomics.ExperimentalAtomicApi
+import kotlin.coroutines.CoroutineContext
+
 /**
  * Entry-point to the KtMongo Multiplatform driver.
  */
-class MongoClient {
+@OptIn(LowLevelApi::class)
+class MongoClient internal constructor(
+	internal val wire: MongoWireClient,
+	val factory: BsonFactory,
+	val context: BsonContext,
+) {
 
 }
+
+/**
+ * Connects to the database at the specified [hostname] and [port].
+ *
+ * By default, connects to `"mongo://localhost:27017"`.
+ *
+ * ### Example
+ *
+ * ```kotlin
+ * val job = Job()
+ * val client = MongoClient(coroutineContext = job)
+ * val collection = client.database("mydb").collection<User>("mycollection")
+ *
+ * println(collection.count())
+ *
+ * job.cancel("Shutting down the client")
+ * ```
+ *
+ * @param coroutineContext The coroutine context used to maintain the connection, including background tasks.
+ * Specify a custom [Job] to control the lifecycle of the client (call [Job.cancel] to close the client).
+ * @param bsonFactory The [BsonFactory] instance used to serialize and deserialize BSON values.
+ * Pass a custom instance to configure polymorphic serialization and other matters.
+ * @param objectIdGenerator The algorithm used to generate new [ObjectId] instances.
+ * @param propertyNameStrategy The algorithm used to convert from the DSL path syntax accesses
+ * to MongoDB field paths.
+ */
+@ExperimentalAtomicApi
+@OptIn(LowLevelApi::class)
+suspend fun MongoClient(
+	hostname: String = "localhost",
+	port: Int = 27017,
+	coroutineContext: CoroutineContext,
+	bsonFactory: BsonFactory = BsonFactory(),
+	objectIdGenerator: ObjectIdGenerator = ObjectIdGenerator.Default(),
+	propertyNameStrategy: PropertyNameStrategy = PropertyNameStrategy.Default,
+): MongoClient = MongoClient(
+	wire = MongoWireClient(
+		hostname,
+		port,
+		bsonFactory,
+		coroutineContext,
+	),
+	factory = bsonFactory,
+	context = BsonContext(bsonFactory, objectIdGenerator, propertyNameStrategy),
+)
