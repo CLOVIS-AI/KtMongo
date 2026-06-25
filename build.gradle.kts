461555fa235417bcff0fa9238d624e332635f08d
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2026-06-25 11:51:46 +0200

    docs(driver-multiplatform): Include the Multiplatform driver in the documentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025, OpenSavvy and contributors.
+ * Copyright (c) 2025-2026, OpenSavvy and contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -54,6 +54,8 @@ dependencies {
 	library(projects.driverSyncKmongo)
 	library(projects.driverCoroutines)
 	library(projects.driverCoroutinesKmongo)
+	library(projects.driverMultiplatformWire)
+	library(projects.driverMultiplatform)
 }
 
 // region Check the users of the project didn't forget to rename the group
