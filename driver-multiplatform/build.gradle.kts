09638957cace0a8c42335fcaa1240163f858f77b
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2026-09-04 21:07:13 +0200

    build(driver-multiplatform): Re-enable the tests

--- driver-multiplatform/build.gradle.kts
+++ driver-multiplatform/build.gradle.kts
@@ -73,11 +73,3 @@ library {
 
 	// coverage.set(50) // TODO: Increase in the future
 }
-
-// TODO: re-enable tests
-@Suppress("UnstableApiUsage")
-tasks.configureEach {
-	if (name.endsWith("test", ignoreCase = true)) {
-		onlyIf("Tests for the Multiplatform driver have been temporarily disabled until its closing behavior is stabilized") { false }
-	}
-}
